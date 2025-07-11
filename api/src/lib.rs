efd5355ceb90fac06266dacfeca08d4acce22dd2
Tom <josephdotson@belkonar.com>
2025-07-11 08:47:54 -0400

    hi

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -4,12 +4,10 @@ use whynot_errors::{SetupError, SetupResult};
 mod models;
 mod routes;
 
-pub async fn run(conn_str: String) -> SetupResult {
-    // build our application with a single route
+pub async fn run(conn_str: String, host: String) -> SetupResult {
     let app = routes();
 
-    // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+    let listener = tokio::net::TcpListener::bind(host)
         .await
         .map_err(SetupError::new)?;
 
--- api/src/main.rs
+++ api/src/main.rs
@@ -10,6 +10,7 @@ fn require_env(key: &'static str) -> Result<String, SetupError> {
 #[tokio::main]
 async fn main() -> SetupResult {
     let conn_str = require_env("CONN_STR")?;
+    let host = require_env("HOST")?;
 
-    return run(conn_str).await;
+    return run(conn_str, host).await;
 }
