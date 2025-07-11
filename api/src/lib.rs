093bd02dfb94419e580caee3209951ea6e62ffea
Tom <josephdotson@belkonar.com>
2025-07-11 07:39:05 -0400

    more bootstrapping

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -4,12 +4,15 @@ use whynot_errors::{SetupError, SetupResult};
 mod models;
 mod routes;
 
-pub async fn run() -> SetupResult {
+pub async fn run(conn_str: String) -> SetupResult {
     // build our application with a single route
     let app = routes();
 
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+        .await
+        .map_err(SetupError::new)?;
+
     axum::serve(listener, app).await.map_err(SetupError::new)?;
     Ok(())
 }
--- api/src/main.rs
+++ api/src/main.rs
@@ -1,7 +1,15 @@
+use std::{env, str};
+
 use api::run;
-use whynot_errors::SetupResult;
+use whynot_errors::{SetupError, SetupResult};
+
+fn require_env(key: &'static str) -> Result<String, SetupError> {
+    env::var(key).map_err(|_| SetupError::new(format!("ENV '{key}' missing")))
+}
 
 #[tokio::main]
 async fn main() -> SetupResult {
-    return run().await;
+    let conn_str = require_env("CONN_STR")?;
+
+    return run(conn_str).await;
 }
