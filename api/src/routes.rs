b272fafa81e114e3bdabfd8d877fd13272a0a4e6
Tom <josephdotson@belkonar.com>
2025-07-10 22:00:25 -0400

    more

--- api/src/routes.rs
+++ api/src/routes.rs
@@ -1,7 +1,15 @@
-use axum::{Router, routing::get};
+use axum::{Router, http::StatusCode, routing::get};
+use whynot_errors::AppError;
+
+async fn not_found() -> AppError {
+    AppError {
+        code: StatusCode::NOT_FOUND,
+        message: "Not Found".to_string(),
+    }
+}
 
 fn api_routes() -> Router {
-    Router::new()
+    Router::new().fallback(not_found)
 }
 
 pub fn routes() -> Router {
